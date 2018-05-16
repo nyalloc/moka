210fec684bcad7d779ebb03cc49a3a6a2425b984
Stuart Adams <stuartdadams@gmail.com>
2018-05-16 18:58:49 +0100

    wip is_any_of

--- src/includes/type_traits/is_any_of.hpp
+++ src/includes/type_traits/is_any_of.hpp
@@ -2,24 +2,24 @@
 
 namespace loki
 {
-    template<typename... Args>
-    struct is_any_of;
-
-    template<class _Ty1, class _Ty2>
-    struct is_any_of<_Ty1, _Ty2>
+    namespace detail
     {
-        static constexpr bool value = false;
-    };
+        template<class _Ty1, class _Ty2>
+        struct is_any_of<_Ty1, _Ty2>
+        {
+            static constexpr bool value = false;
+        };
 
-    template<class _Ty1>
-    struct is_any_of<_Ty1, _Ty1>
-    {
-        static constexpr bool value = true;
-    };
+        template<class _Ty1>
+        struct is_any_of<_Ty1, _Ty1>
+        {
+            static constexpr bool value = true;
+        };
+    }
 
     template<class _Ty1, class _Ty2, typename... Args>
-    struct is_any_of<_Ty1, _Ty2, Args...>
+    struct is_any_of
     {
-        static constexpr bool value = is_any_of<_Ty1, _Ty2>::value || is_any_of<_Ty1, Args...>::value;
+        static constexpr bool value = detail::is_any_of<_Ty1, _Ty2>::value || is_any_of<_Ty1, Args...>::value;
     };
 }
