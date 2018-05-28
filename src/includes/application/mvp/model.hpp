1ae8880808a8db24c63cd3f55faac3a2fe1195df
Stuart Adams <stuartdadams@live.com>
2018-05-28 15:45:56 +0100

    notify property framework updated

--- src/includes/application/mvp/model.hpp
+++ src/includes/application/mvp/model.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include <string>
@@ -7,190 +6,227 @@
 #include <memory>
 #include <iostream>
 
-namespace loki
+template<typename T>
+using subscriber = std::function<void(const T&)>;
+
+struct base_subscriber_wrapper
 {
-    struct base_function_wrapper
+    base_subscriber_wrapper() = default;
+    base_subscriber_wrapper(const base_subscriber_wrapper&) noexcept = delete;
+    base_subscriber_wrapper(const base_subscriber_wrapper&&) noexcept = delete;
+    base_subscriber_wrapper& operator=(const base_subscriber_wrapper&) noexcept = delete;
+    base_subscriber_wrapper& operator=(const base_subscriber_wrapper&&) noexcept = delete;
+
+    virtual ~base_subscriber_wrapper() = default;
+};
+
+template<class T>
+struct subscriber_wrapper : base_subscriber_wrapper
+{
+    subscriber<T> function;
+
+    subscriber_wrapper(const subscriber<T>& function)
+        : function(function)
+    {}
+
+    subscriber_wrapper() = default;
+    subscriber_wrapper(const subscriber_wrapper&) noexcept = delete;
+    subscriber_wrapper(const subscriber_wrapper&&) noexcept = delete;
+    subscriber_wrapper& operator=(subscriber_wrapper&&) noexcept = delete;
+    subscriber_wrapper& operator=(const subscriber_wrapper&) noexcept = delete;
+
+    virtual ~subscriber_wrapper() = default;
+};
+
+using property_id = uint32_t;
+
+template<class T>
+class property;
+
+class property_changed
+{
+    std::unordered_multimap<property_id, std::shared_ptr<base_subscriber_wrapper>> subscribers;
+    property_id available_ids = 0;
+public:
+    virtual ~property_changed() = default;
+
+    property_id get_new_id()
     {
-        virtual ~base_function_wrapper() = default;
-    };
+        return ++available_ids;
+    }
 
     template<class T>
-    struct function_wrapper : base_function_wrapper
-    {
-        std::function<void(const T&)> function;
-
-        explicit function_wrapper(const std::function<void(const T&)>& function)
-            : function(function)
-        {}
-
-        virtual ~function_wrapper() = default;
-    };
-
-    class property_changed
-    {
-        std::unordered_multimap<std::string, std::shared_ptr<base_function_wrapper>> properties;
-
-    public:
-        template<class T>
-        void notify_property_changed(const std::string& name, const T& value)
-        {
-            const auto range = properties.equal_range(name);
-            for (auto it = range.first; it != range.second; ++it)
-            {
-                const auto base = it->second;
-                auto derived = std::dynamic_pointer_cast<function_wrapper<T>>(base);
-                derived->function(value);
-            }
-        }
-
-        template<class T>
-        void on_property_changed(const std::string& name, const std::function<void(const T&)>& slot)
-        {
-            auto wrapper = std::make_shared<function_wrapper<T>>(slot);
-            properties.emplace(name, wrapper);
-        }
-    };
-
-    class base_model : public property_changed
-    {
-
-    };
-
-    class base_view : public property_changed
-    {
-    };
+    void notify_property_changed(const property<T>& name);
 
     template<class T>
-    class property
+    void on_property_changed(const property<T>& name, const subscriber<T>& slot);
+};
+
+class base_presenter
+{
+};
+
+template<class T>
+class property
+{
+    property_id id;
+    T& value;
+    property_changed* notifier;
+public:
+    const T& get_value() const noexcept
     {
-        std::string name;
-        T value;
-        property_changed* notifier;
-    public:
-        const T& get_value() const noexcept
-        {
-            return value;
-        }
-
-        void set_value(const T& value, const bool notify = true)
-        {
-            this->value = value;
-            if (notify)
-            {
-                this->notifier->template notify_property_changed<T>(name, value);
-            }
-        }
-
-        const std::string& get_name() const noexcept
-        {
-            return name;
-        }
-
-        property(const std::string& name, const T& value, property_changed* notifier)
-            : name(name), value(value), notifier(notifier)
-        {
-            this->notifier->template notify_property_changed<T>(name, value);
-        }
-
-        property<T>& operator=(const T& value) noexcept
-        {
-            this->value = value;
-            this->notifier->template notify_property_changed<T>(name, value);
-            return *this;
-        }
-
-        property<T>& operator=(T&& value) noexcept
-        {
-            this->value = std::move(value);
-            this->notifier->template notify_property_changed<T>(name, value);
-            return *this;
-        }
-    };
-
-    class concrete_model : public base_model
-    {
-        property<int> count;
-    public:
-        concrete_model()
-            : count{ "count", 0, this }
-        {}
-    };
-
-    template<typename model_type, typename view_type>
-    class concrete_presenter;
-
-    class concrete_view : public base_view
-    {
-        concrete_presenter<concrete_model, concrete_view>* presenter = nullptr;
-        property<int> count;
-    public:
-        concrete_view()
-            : count{ "count", 0, this }
-        {}
-
-        void set_presenter(concrete_presenter<concrete_model, concrete_view>* presenter);
-
-        void update();
-
-        void draw() const;
-    };
-
-    struct presenter_messager
-    {
-        template<typename model_type, typename view_type>
-        presenter_messager(model_type& model, view_type& view)
-        {}
-    };
-
-    template<typename model_type, typename view_type>
-    class concrete_presenter
-    {
-        model_type& model;
-        view_type& view;
-    public:
-        concrete_presenter<model_type, view_type>(model_type& model, view_type& view)
-            : model(model), view(view)
-        {
-            this->view.set_presenter(this);
-
-            this->model.template on_property_changed<int>("count", [this](const int& count)
-            {
-                std::cout << "Model state updated, notifying view" << std::endl;
-                this->view.count.set_value(count, false);
-            });
-
-            this->view.template on_property_changed<int>("count", [this](const int& count)
-            {
-                std::cout << "View state updated, notifying model" << std::endl;
-                this->model.count.set_value(count, false);
-            });
-        }
-    };
-
-    inline void concrete_view::set_presenter(concrete_presenter<concrete_model, concrete_view>* presenter)
-    {
-        this->presenter = presenter;
+        return value;
     }
 
-    inline void concrete_view::update()
+    void set_value(const T& value, const bool notify = true)
     {
-        auto value = count.get_value();
-        count.set_value(++value);
+        this->value = value;
+        if (notify)
+        {
+            this->notifier->notify_property_changed<T>(*this);
+        }
     }
 
-    inline void concrete_view::draw() const
+    property_id get_id() const noexcept
     {
-        std::cout << count.get_value() << std::endl;
+        return id;
     }
+
+    property(T& value, property_changed* notifier)
+        : id(notifier->get_new_id()), value(value), notifier(notifier)
+    {
+        this->notifier->notify_property_changed<T>(*this);
+    }
+
+    property<T>& operator=(const T& value) noexcept
+    {
+        this->value = value;
+        this->notifier->notify_property_changed<T>(*this);
+        return *this;
+    }
+
+    property<T>& operator=(T&& value) noexcept
+    {
+        this->value = std::move(value);
+        this->notifier->notify_property_changed<T>(*this);
+        return *this;
+    }
+};
+
+template<class T>
+void property_changed::notify_property_changed(const property<T>& property)
+{
+    // get all subscribers who are subscribed to this property
+    const auto range = subscribers.equal_range(property.get_id());
+
+    // iterate over all subscribers and notify them of the property's new state
+    for (auto it = range.first; it != range.second; ++it)
+    {
+        const auto base = it->second;
+        auto derived = std::dynamic_pointer_cast<subscriber_wrapper<T>>(base);
+        derived->function(property.get_value());
+    }
+}
+
+template<class T>
+void property_changed::on_property_changed(const property<T>& property, const subscriber<T>& slot)
+{
+    auto wrapper = std::make_shared<subscriber_wrapper<T>>(slot);
+    subscribers.emplace(property.get_id(), wrapper);
+}
+
+class base_model : public property_changed
+{
+    int count_ = 0;
+public:
+    property<int> count;
+
+    base_model()
+        : count{ count_, this }
+    {}
+};
+
+class base_view : public property_changed
+{
+    int count_ = 0;
+public:
+    property<int> count;
+
+    base_view()
+        : count{ count_, this }
+    {}
+
+    virtual void set_presenter(base_presenter* presenter) = 0;
+};
+
+class concrete_model : public base_model
+{
+};
+
+class concrete_view : public base_view
+{
+    base_presenter* presenter = nullptr;
+public:
+    concrete_view()
+        : base_view()
+    {}
+
+    void set_presenter(base_presenter* presenter) override;
+
+    void update();
+
+    void draw() const;
+};
+
+class concrete_presenter : public base_presenter
+{
+    base_model& model;
+    base_view& view;
+public:
+    concrete_presenter(base_model& model, base_view& view)
+        : model(model), view(view)
+    {
+        this->view.set_presenter(this);
+
+        this->model.on_property_changed<int>(model.count, 
+            [this](const auto& count)
+        {
+            std::cout << "Model state updated, notifying view" << std::endl;
+            this->view.count.set_value(count, false);
+        });
+
+        this->view.on_property_changed<int>(view.count,
+            [this](const auto& count)
+        {
+            std::cout << "View state updated, notifying model" << std::endl;
+            this->model.count.set_value(count, false);
+        });
+    }
+};
+
+inline void concrete_view::set_presenter(base_presenter* presenter)
+{
+    this->presenter = presenter;
+}
+
+inline void concrete_view::update()
+{
+    auto value = count.get_value();
+    count.set_value(++value);
+}
+
+inline void concrete_view::draw() const
+{
+    std::cout << count.get_value() << std::endl;
 }
 
 inline void test()
 {
-    loki::concrete_model model;
+    concrete_model model;
 
-    loki::concrete_view view;
+    concrete_view view;
 
-    loki::concrete_presenter<loki::concrete_model, loki::concrete_view> presenter
+    concrete_presenter presenter
     {
         model,
         view
@@ -202,5 +238,10 @@ inline void test()
     {
         view.update();
         view.draw();
+
+        if (model.count.get_value() == 100)
+        {
+            model.count.set_value(0);
+        }
     }
 }
