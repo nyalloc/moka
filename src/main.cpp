3c2aa814af966a0cb70c26099092b72bfb69a91e
Stuart Adams <stuartdadams@live.com>
2018-06-07 00:22:02 +0100

    Vector class in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 
 #include <application/application.hpp>
+#include "maths/vector.hpp"
 
 int main(int argc, char* argv[])
 {
+    constexpr loki::vector3 position { 2, 2 };
+
+    constexpr loki::vector3 position2 { 2, 2, 2 };
+
+    constexpr loki::vector3 position3 = cross(position, position2);
+
     loki::application app{ argc, argv };
     return app.run();
 }
