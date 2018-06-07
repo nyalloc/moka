e3553b9183e848f733245d66454ccb1bd19ff234
Stuart Adams <stuartdadams@live.com>
2018-06-07 23:23:15 +0100

    Updated vector class capabilities

--- src/includes/maths/vector.hpp
+++ src/includes/maths/vector.hpp
@@ -26,7 +26,7 @@ namespace loki
         return static_cast<float>(val) / 0xFF;
     }
 
-    template<typename T, size_t size, typename = std::enable_if<std::is_arithmetic<T>::value>>
+    template<typename T, size_t size, bool is_colour = false, typename = std::enable_if_t<std::is_arithmetic<T>::value>>
     class vector
     {
         constexpr static T epsilon = std::numeric_limits<T>::epsilon();
@@ -35,17 +35,17 @@ namespace loki
 
     public:
 
-        template<typename = std::enable_if<size >= 2>>
+        template<typename = std::enable_if_t<size >= 2>>
         constexpr vector(const T x, const T y) noexcept
             : components{ x, y }
         {}
 
-        template<typename = std::enable_if<size >= 3>>
+        template<typename = std::enable_if_t<size >= 3>>
         constexpr vector(const T x, const T y, const T z) noexcept
             : components{ x, y, z }
         {}
 
-        template<typename = std::enable_if<size == 4>>
+        template<typename = std::enable_if_t<size == 4>>
         constexpr vector(const T x, const T y, const T z, const T w) noexcept
             : components{ x, y, z, w }
         {}
@@ -55,43 +55,43 @@ namespace loki
             return components[0];
         }
 
-        template<typename = std::enable_if<size >= 2>>
+        template<typename = std::enable_if_t<size >= 2>>
         constexpr T y() const noexcept
         {
             return components[1];
         }
 
-        template<typename = std::enable_if<size >= 3>>
+        template<typename = std::enable_if_t<size >= 3>>
         constexpr T z() const noexcept
         {
             return components[2];
         }
 
-        template<typename = std::enable_if<size >= 4>>
+        template<typename = std::enable_if_t<size >= 4>>
         constexpr T w() const noexcept
         {
             return components[3];
         }
 
-        template<typename = std::enable_if<std::is_same_v<T, uint8_t>>>
+        template<typename = std::enable_if_t<is_colour>>
         constexpr float r() const noexcept
         {
             return to_float(components[0]);
         }
 
-        template<typename = std::enable_if<size >= 2 && std::is_same_v<T, uint8_t>>>
+        template<typename = std::enable_if_t<size >= 2 && is_colour>>
         constexpr float g() const noexcept
         {
             return to_float(components[1]);
         }
 
-        template<typename = std::enable_if<size >= 3 && std::is_same_v<T, uint8_t>>>
+        template<typename = std::enable_if_t<size >= 3 && is_colour>>
         constexpr float b() const noexcept
         {
             return to_float(components[2]);
         }
 
-        template<typename = std::enable_if<size >= 4 && std::is_same_v<T, uint8_t>>>
+        template<typename = std::enable_if_t<size >= 4 && is_colour>>
         constexpr float a() const noexcept
         {
             return to_float(components[3]);
@@ -120,7 +120,7 @@ namespace loki
         return lhs;
     }
 
-    template<typename T, size_t size, typename = typename std::enable_if<size == 3>::type>
+    template<typename T, size_t size, typename = std::enable_if_t<size == 3>>
     constexpr static vector<T, size> cross(const vector<T, size>& lhs, const vector<T, size>& rhs) noexcept
     {
         return
