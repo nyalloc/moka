199a4f403c327208a171a87a8858e5f4930e851e
Stuart Adams <stuartdadams@live.com>
2018-06-07 22:27:38 +0100

    Matrix class in progress

---
+++ src/includes/maths/matrix.hpp
@@ -0,0 +1,65 @@
+#include <array>
+#include <utility>
+
+namespace loki
+{
+    template<typename T, size_t row, size_t column>
+    class matrix
+    {
+        using column_type = std::array<T, column>;
+        using row_type = std::array<T, row>;
+        using type = matrix<T, row, column>;
+        using transpose_type = matrix<T, column, row>;
+        using value_type = T;
+
+        std::array<column_type, column> rows;
+
+    public:
+        constexpr column_type& operator[](const size_t index)
+        {
+            return rows[index];
+        }
+
+        constexpr const column_type& operator[](const size_t index) const
+        {
+            return rows[index];
+        }
+
+        constexpr void populate(size_t c, size_t r, T&& first) noexcept
+        {
+            rows[c][r] = first;
+        }
+
+        template<typename... Args>
+        constexpr void populate(size_t c, size_t r, T&& first, Args&&... args)  noexcept
+        {
+            rows[c][r] = first;
+
+            if (c != column - 1)
+            {
+                populate(++c, r, std::forward<T>(args)...);
+            }
+            else
+            {
+                populate(0, ++r, std::forward<T>(args)...);
+            }
+        }
+
+        template<typename... Args>
+        constexpr matrix(Args&&... args) noexcept
+            : rows{}
+        {
+            populate(0, 0, std::forward<T>(args)...);
+        }
+    };
+
+    using matrix3 = matrix<float, 3, 3>;
+    using matrix3x3 = matrix3;
+    using mat3 = matrix3;
+    using mat3x3 = matrix3;
+
+    using matrix4 = matrix<float, 4, 4>;
+    using matrix4x4 = matrix4;
+    using mat4 = matrix4;
+    using mat4x4 = matrix4;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 
 #include <application/application.hpp>
 #include "maths/vector.hpp"
+#include "maths/matrix.hpp"
 
 int main(int argc, char* argv[])
 {
-    constexpr loki::vector3 position { 2, 2 };
+    constexpr loki::vector3 position { 1, 0, 0 };
 
-    constexpr loki::vector3 position2 { 2, 2, 2 };
+    constexpr loki::vector3 position2 { 0, 0, 1 };
 
-    constexpr loki::vector3 position3 = cross(position, position2);
+    constexpr loki::vector3 position3 = loki::cross(position, position2);
+
+    constexpr loki::matrix4 matrix
+    {
+        1.0f,  2.0f,  3.0f,  4.0f,
+        5.0f,  6.0f,  7.0f,  8.0f,
+        9.0f,  10.0f, 11.0f, 12.0f,
+        13.0f, 14.0f, 15.0f, 16.0f
+    };
 
     loki::application app{ argc, argv };
     return app.run();
