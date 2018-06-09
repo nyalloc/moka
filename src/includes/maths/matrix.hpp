1429775d15b7831b1fe78e663a08c5ad66ec771d
Stuart Adams <stuartdadams@live.com>
2018-06-09 01:38:35 +0100

    Matrix multiplication

--- src/includes/maths/matrix.hpp
+++ src/includes/maths/matrix.hpp
@@ -3,37 +3,37 @@
 
 namespace loki
 {
-    template<typename T, size_t row, size_t column>
+    template<typename T, size_t column, size_t row>
     class matrix
     {
-        using column_type = std::array<T, column>;
-        using row_type = std::array<T, row>;
-        using type = matrix<T, row, column>;
-        using transpose_type = matrix<T, column, row>;
+        using column_type = std::array<T, row>;
+        using row_type = std::array<T, column>;
+        using type = matrix<T, column, row>;
+        using transpose_type = matrix<T, row, column>;
         using value_type = T;
 
-        std::array<column_type, column> rows;
+        std::array<column_type, column> columns;
 
     public:
         constexpr column_type& operator[](const size_t index)
         {
-            return rows[index];
+            return columns[index];
         }
 
         constexpr const column_type& operator[](const size_t index) const
         {
-            return rows[index];
+            return columns[index];
         }
 
         constexpr void populate(size_t c, size_t r, T&& first) noexcept
         {
-            rows[c][r] = first;
+            columns[c][r] = first;
         }
 
         template<typename... Args>
         constexpr void populate(size_t c, size_t r, T&& first, Args&&... args)  noexcept
         {
-            rows[c][r] = first;
+            columns[c][r] = first;
 
             if (c != column - 1)
             {
@@ -45,22 +45,38 @@ namespace loki
             }
         }
 
+        constexpr matrix() noexcept
+            : columns{}
+        {}
+
         template<typename... Args>
-        constexpr matrix(Args&&... args) noexcept
-            : rows{}
+        constexpr matrix(T&& first, Args&&... args) noexcept
+            : columns{}
         {
-            populate(0, 0, std::forward<T>(args)...);
+            populate(0, 0, std::forward<T>(first), std::forward<T>(args)...);
         }
     };
 
-    template<typename T, size_t row, size_t column, typename T2, size_t row2, size_t column2>
-    constexpr auto operator * (const matrix<T, row, column>& lhs, const matrix<T2, row2, column2>& rhs)
+    template<typename T, size_t m1_columns, size_t m1_rows, size_t m2_columns, size_t m2_rows>
+    constexpr matrix<T, m2_columns, m1_rows> operator * (const matrix<T, m1_columns, m1_rows>& lhs, const matrix<T, m2_columns, m2_rows>& rhs)
     {
-        static_assert(column == row2, "Cannot multiply these matrices. Please ensure the number of columns in the first matrix == the number of rows in the second matrix.");
+        static_assert(m1_columns == m2_rows, "Cannot multiply these matrices. Please ensure the number of columns in the first matrix == the number of rows in the second matrix.");
 
+        matrix<T, m2_columns, m1_rows> product;
 
+        for (size_t i = 0; i < m1_rows; ++i)
+        {
+            for (size_t j = 0; j < m2_columns; ++j)
+            {
+                product[j][i] = 0;
+                for (size_t k = 0; k < m1_columns; ++k)
+                {
+                    product[j][i] = product[j][i] + (lhs[k][i] * rhs[j][k]);
+                }
+            }
+        }
 
-        return lhs;
+        return product;
     }
 
     using matrix3 = matrix<float, 3, 3>;
@@ -72,4 +88,10 @@ namespace loki
     using matrix4x4 = matrix4;
     using mat4 = matrix4;
     using mat4x4 = matrix4;
+
+    using matrix2x3 = matrix<float, 2, 3>;
+    using mat2x3 = matrix2x3;
+
+    using matrix3x2 = matrix<float, 3, 2>;
+    using mat3x2 = matrix3x2;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,20 @@ int main(int argc, char* argv[])
 
     constexpr loki::vector3 position3 = loki::cross(position, position2);
 
-    constexpr loki::matrix4 mat4
+    loki::matrix2x3 mat2x3
     {
-        1.0f, 0.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f, 0.0f,
-        0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f
+        2.0f, 4.0f,
+        6.0f, 3.0f,
+        1.0f, 2.0f,
     };
 
-    constexpr loki::matrix3 mat3
+    loki::matrix3x2 mat3x2
     {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f,
+        3.0f, 4.0f, 1.0f,
+        3.0f, 3.0f, 8.0f
     };
+
+    auto result = mat2x3 * mat3x2;
 
     loki::application app{ argc, argv };
     return app.run();
